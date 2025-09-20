f6bba52f5feea42a8133addcf192bd7a597fa716
davidvivascamargo <jesusdavid.vc98@gmail.com>
2025-09-19 20:33:06 -0600

    Agregar migraciones automáticas en wsgi.py

--- webempresa/wsgi.py
+++ webempresa/wsgi.py
@@ -5,7 +5,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
+
 
 import os
 
@@ -13,4 +13,33 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webempresa.settings")
 
+application = get_wsgi_application()
+"""
+"""
+WSGI config for webempresa project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+import django
+from django.core.management import call_command
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webempresa.settings")
+
+# Inicializamos Django
+django.setup()
+
+# Ejecutamos migraciones automáticamente
+try:
+    call_command('migrate', interactive=False)
+    print("Migraciones aplicadas con éxito")
+except Exception as e:
+    print(f"Error al correr migraciones: {e}")
+
+# Levantamos la aplicación
 application = get_wsgi_application()
