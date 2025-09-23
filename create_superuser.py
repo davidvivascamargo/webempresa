5703de9b70b3442f41c2ed6445180c04d61e1a85
davidvivascamargo <jesusdavid.vc98@gmail.com>
2025-09-23 12:45:53 -0600

    Agregar script de creación de superusuario

--- create_superuser.py
+++ create_superuser.py
@@ -1,7 +1,16 @@
-# create_superuser.py
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webempresa.settings")
+
+django.setup()
+
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser("admin", "admin@example.com", "adminpassword")
+    User.objects.create_superuser("admin", "admin@example.com", "admin123")
+    print("✅ Superusuario creado: admin / admin123")
+else:
+    print("ℹ️ El superusuario ya existe.")
