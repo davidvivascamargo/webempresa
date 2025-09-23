3f631eb706f058ce20fb78b404cd2d065a6a874f
davidvivascamargo <jesusdavid.vc98@gmail.com>
2025-09-23 13:34:08 -0600

    Fix: import os to serve media/static files

--- webempresa/urls.py
+++ webempresa/urls.py
@@ -19,6 +19,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+import os
+
 
 urlpatterns = [
     # Paths del core
