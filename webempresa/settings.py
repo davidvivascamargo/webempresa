5d3ce86d9431a1e74477136cc37f3abd2de01f2a
davidvivascamargo <jesusdavid.vc98@gmail.com>
2025-09-23 19:12:41 -0600

    Config: conexión a PostgreSQL en Render + cambio de imagen

--- webempresa/settings.py
+++ webempresa/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import django
+
 
 import dj_database_url
 
@@ -81,14 +83,22 @@ WSGI_APPLICATION = "webempresa.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+'''
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 
-import django
 django.setup()
 
 
